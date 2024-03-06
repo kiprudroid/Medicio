26f302d9419bb3812b18db4aab9a5246f3e5dd56
kiprudroid <kiprutoshadrack06@gmail.com>
2024-03-06 13:05:11 +0300

    Mpesa Api

---
+++ Blog/templates/quote.md
@@ -0,0 +1,24 @@
+I am looking for an experienced Full Stack developer who can create an e-commerce like site based on Node.js backend and React.js frontend with database as MySQL (I am ok with Laravel backend but if you can commit website speed) with all standard security features and with some custom features. Specifically, the custom feature I want is a customized "add to cart" feature that allows multiple users to buy together for a group deal.
+
+
+Key features you will need to implement include but are not limited to:
+
+- Need Mobile phone and website both views.
+- SEO friendly URLs
+- Easy One page checkout process
+- User and Vendors login using a phone number and OTP
+- Payment gateway integration
+- Admin dashboard --( Full privilege)
+- Supervisor dashboard --( Limited privilege)
+- Vendor dashboard ---- ( Limited privilege)
+- User dashboard ---- ( limited privilege)
+- Vendors should also be able to post new deals.
+- User should be able to drag and move "product category" icons and put them in order on home screen according to per their preference.
+- frontend users should be able to share products on social media (with respective OG Tags). On Instagram Whatsapp and FB etc.
+
+
+Ideal Skills and Experience:
+In terms of skills and experience, you need to have proven experience in website development, particularly in Node.js and React.js in creating e-commerce sites. Prior experience in integrating a 'group buy' feature or similar functionality would be a compelling advantage. A strong knack delivery of efficient, clean and secure code is highly valued in this project.
+
+Timeline:
+I expect the project to be completed within a month .
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,11 @@
+import json
 from django.shortcuts import render, redirect
+from mechanize import HTTPBasicAuthHandler
+import requests
+from Blog.credentials import LipanaMpesaPpassword, MpesaAccessToken
 from Blog.models import Members,Response, Users ,ImageModel
 from Project.forms import ImageUploadForm
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -64,3 +69,44 @@ def imagedelete(request, id):
     image = ImageModel.objects.get(id=id)
     image.delete()
     return redirect('/showimage')
+
+def token(request):
+    consumer_key = 'ePeSz5tnbI0k3fhj6AFiitFU5yAmRfDIPO16nBSPsIrSy5dH'
+    consumer_secret = 'QNm2CQsgyYqZAsu0DUja6G3vNyTTdGHd0jYG11F9QoEsEJeCDenJePLSEI1eTE8j'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+
+    r = requests.get(api_URL, auth=HTTPBasicAuthHandler(
+        consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token["access_token"]
+
+    return render(request, 'token.html', {"token":validated_mpesa_access_token})
+
+def pay(request):
+   return render(request, 'pay.html')
+
+
+
+def stk(request):
+    if request.method =="POST":
+        phone = request.POST['phone']
+        amount = request.POST['amount']
+        access_token = MpesaAccessToken.validated_mpesa_access_token
+        api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        headers = {"Authorization": "Bearer %s" % access_token}
+        request = {
+            "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+            "Password": LipanaMpesaPpassword.decode_password,
+            "Timestamp": LipanaMpesaPpassword.lipa_time,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phone,
+            "PartyB": LipanaMpesaPpassword.Business_short_code,
+            "PhoneNumber": phone,
+            "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+            "AccountReference": "Apen Softwares",
+            "TransactionDesc": "Web Development Charges"
+        }
+        response = requests.post(api_url, json=request, headers=headers)
+        return HttpResponse(response)
+
